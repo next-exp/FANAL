6787e9cbebbf18a02fc585328a140baabf85f462
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2018-11-21 11:27:01 +0100

    Fix typo.

--- fanal/core/fanal_exceptions.py
+++ fanal/core/fanal_exceptions.py
@@ -5,5 +5,5 @@ Define Fanal-specific exceptions
 class FException(Exception):
     """ Base class for Fanal exceptions hierarchy """
 
-class DetectorNameNotDefined(ICException):
+class DetectorNameNotDefined(FException):
     """ Detector name is not defined """
