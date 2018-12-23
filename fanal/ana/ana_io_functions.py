23a815e5bd57fdba96bbcb431b68dad90c11001f
jmunozv <javier.munoz.vidal@gmail.com>
2018-12-23 12:39:33 +0100

    Tracks lengths added to stored info

--- fanal/ana/ana_io_functions.py
+++ fanal/ana/ana_io_functions.py
@@ -5,61 +5,48 @@ import pandas as pd
 
 
 
-#def get_ana_file_name(path, evt_type):
-#	"""
-#	It returns the name of the analysis file in the 'path'.
-#	"""
-#	try:
-#		os.stat(path)
-#	except:
-#		os.mkdir(path)
-#
-#	fileName = evt_type + '.ana.h5'
-#	fileName = os.path.join(path, fileName)
-#
-#	return fileName
-
-
-
 def get_ana_group_name(fwhm, spatial_def):
-	"""
-	It returns the name of the ana_group where analysis-data is stored.
-	fwhm: FWHM at Qbb in %
-	spatial_def: Spatial definition. ('Std' or 'High')
-	"""
-	assert spatial_def in ['Std', 'High'], '{} is not a valid Spatial Resolution' \
-		.format(spatial_def)
+  """
+  It returns the name of the ana_group where analysis-data is stored.
+  fwhm: FWHM at Qbb in %
+  spatial_def: Spatial definition. ('Std' or 'High')
+  """
+  assert spatial_def in ['Std', 'High'], '{} is not a valid Spatial Resolution' \
+    .format(spatial_def)
 
-	ana_group_name = '/FANALIC/ANA_{0}fmhm_{1}Def' \
-		.format(str(fwhm).replace('.', ''), spatial_def)
+  ana_group_name = '/FANALIC/ANA_{0}fmhm_{1}Def' \
+    .format(str(fwhm).replace('.', ''), spatial_def)
 
-	return ana_group_name
+  return ana_group_name
 
 
 
 def get_events_ana_dict():
-	"""
-	It returns a dictionary with a key for each field to be stored per event
-	during the fanalIC analysis step.
-	The initial values are empty lists.
-	"""
-	events_dict = {
+  """
+  It returns a dictionary with a key for each field to be stored per event
+  during the fanalIC analysis step.
+  The initial values are empty lists.
+  """
+  events_dict = {
 		'id':            [],
-	    'numTracks':     [],
-	    'track0_E':      [],
-	    'track0_voxels': [],
-	    'track1_E':      [],
-	    'track1_voxels': [],
-	    'track2_E':      [],
-	    'track2_voxels': [],
-	    'tracks_filter': [],
-	    'blob1_E':       [],
-	    'blob2_E':       [],
-	    'blobs_filter':  [],
-	    'roi_filter':    []
+    'numTracks':     [],
+    'track0_E':      [],
+    'track0_voxels': [],
+    'track0_length': [],
+    'track1_E':      [],
+    'track1_voxels': [],
+    'track1_length': [],
+    'track2_E':      [],
+    'track2_voxels': [],
+    'track2_length': [],
+    'tracks_filter': [],
+    'blob1_E':       [],
+    'blob2_E':       [],
+    'blobs_filter':  [],
+    'roi_filter':    []
 	}
 
-	return events_dict
+  return events_dict
 
 
 
@@ -85,32 +72,38 @@ def extend_events_ana_data(
 	numTracks,
 	track0_E,
 	track0_voxels,
+  track0_length,
 	track1_E,
 	track1_voxels,
+  track1_length,
 	track2_E,
 	track2_voxels,
+  track2_length,
 	tracks_filter,
 	blob1_E       = np.nan,
 	blob2_E       = np.nan,
 	blobs_filter  = False,
 	roi_filter    = False
 	):
-	"""
-	It stores all the event data from the analysis into the events_dict.
-	"""
-	events_dict['id'].extend([event_id])
-	events_dict['numTracks'].extend([numTracks])
-	events_dict['track0_E'].extend([track0_E])
-	events_dict['track0_voxels'].extend([track0_voxels])
-	events_dict['track1_E'].extend([track1_E])
-	events_dict['track1_voxels'].extend([track1_voxels])
-	events_dict['track2_E'].extend([track2_E])
-	events_dict['track2_voxels'].extend([track2_voxels])
-	events_dict['tracks_filter'].extend([tracks_filter])
-	events_dict['blob1_E'].extend([blob1_E])
-	events_dict['blob2_E'].extend([blob2_E])
-	events_dict['blobs_filter'].extend([blobs_filter])
-	events_dict['roi_filter'].extend([roi_filter])
+  """
+  It stores all the event data from the analysis into the events_dict.
+  """
+  events_dict['id'].extend([event_id])
+  events_dict['numTracks'].extend([numTracks])
+  events_dict['track0_E'].extend([track0_E])
+  events_dict['track0_voxels'].extend([track0_voxels])
+  events_dict['track0_length'].extend([track0_length])
+  events_dict['track1_E'].extend([track1_E])
+  events_dict['track1_voxels'].extend([track1_voxels])
+  events_dict['track1_length'].extend([track1_length])
+  events_dict['track2_E'].extend([track2_E])
+  events_dict['track2_voxels'].extend([track2_voxels])
+  events_dict['track2_length'].extend([track2_length])
+  events_dict['tracks_filter'].extend([tracks_filter])
+  events_dict['blob1_E'].extend([blob1_E])
+  events_dict['blob2_E'].extend([blob2_E])
+  events_dict['blobs_filter'].extend([blobs_filter])
+  events_dict['roi_filter'].extend([roi_filter])
 
 
 
@@ -126,29 +119,32 @@ def extend_voxels_ana_data(voxels_dict, voxels_indexes, voxels_newE, voxels_trac
 
 
 def store_events_ana_data(file_name, group_name, events_df, events_dict):
-	"""
-	Adds the events dictionary data, corresponding to the event_id's passed
-	to the dataFrame. Then dataFrame is stored in
-	file_name / group_name / events.
-	"""
-	events_df.loc[events_dict['id'], 'num_tracks']    = events_dict['numTracks']
-	events_df.loc[events_dict['id'], 'track0_E']      = events_dict['track0_E']
-	events_df.loc[events_dict['id'], 'track0_voxels'] = events_dict['track0_voxels']
-	events_df.loc[events_dict['id'], 'track1_E']      = events_dict['track1_E']
-	events_df.loc[events_dict['id'], 'track1_voxels'] = events_dict['track1_voxels']
-	events_df.loc[events_dict['id'], 'track2_E']      = events_dict['track2_E']
-	events_df.loc[events_dict['id'], 'track2_voxels'] = events_dict['track2_voxels']
-	events_df.loc[events_dict['id'], 'tracks_filter'] = events_dict['tracks_filter']
-	events_df.tracks_filter.fillna(False, inplace = True)
-	events_df.loc[events_dict['id'], 'blob1_E']       = events_dict['blob1_E']
-	events_df.loc[events_dict['id'], 'blob2_E']       = events_dict['blob2_E']
-	events_df.loc[events_dict['id'], 'blobs_filter']  = events_dict['blobs_filter']
-	events_df.blobs_filter.fillna(False, inplace = True)
-	events_df.loc[events_dict['id'], 'roi_filter']    = events_dict['roi_filter']
-	events_df.roi_filter.fillna(False, inplace = True)
+  """
+  Adds the events dictionary data, corresponding to the event_id's passed
+  to the dataFrame. Then dataFrame is stored in
+  file_name / group_name / events.
+  """
+  events_df.loc[events_dict['id'], 'num_tracks']    = events_dict['numTracks']
+  events_df.loc[events_dict['id'], 'track0_E']      = events_dict['track0_E']
+  events_df.loc[events_dict['id'], 'track0_voxels'] = events_dict['track0_voxels']
+  events_df.loc[events_dict['id'], 'track0_length'] = events_dict['track0_length']
+  events_df.loc[events_dict['id'], 'track1_E']      = events_dict['track1_E']
+  events_df.loc[events_dict['id'], 'track1_voxels'] = events_dict['track1_voxels']
+  events_df.loc[events_dict['id'], 'track1_length'] = events_dict['track1_length']
+  events_df.loc[events_dict['id'], 'track2_E']      = events_dict['track2_E']
+  events_df.loc[events_dict['id'], 'track2_voxels'] = events_dict['track2_voxels']
+  events_df.loc[events_dict['id'], 'track2_length'] = events_dict['track2_length']
+  events_df.loc[events_dict['id'], 'tracks_filter'] = events_dict['tracks_filter']
+  events_df.tracks_filter.fillna(False, inplace = True)
+  events_df.loc[events_dict['id'], 'blob1_E']       = events_dict['blob1_E']
+  events_df.loc[events_dict['id'], 'blob2_E']       = events_dict['blob2_E']
+  events_df.loc[events_dict['id'], 'blobs_filter']  = events_dict['blobs_filter']
+  events_df.blobs_filter.fillna(False, inplace = True)
+  events_df.loc[events_dict['id'], 'roi_filter']    = events_dict['roi_filter']
+  events_df.roi_filter.fillna(False, inplace = True)
 
-	events_df.sort_index()
-	events_df.to_hdf(file_name, group_name + '/events', format='table', data_columns=True)
+  events_df.sort_index()
+  events_df.to_hdf(file_name, group_name + '/events', format='table', data_columns=True)
 
 
 
