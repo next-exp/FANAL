4961becdac14a9b6e132d3497a7b3eba7007a930
jmunozv <javier.munoz.vidal@gmail.com>
2021-03-04 18:24:50 +0100

    Update mc_utils printing functions

--- fanal/utils/mc_utils.py
+++ fanal/utils/mc_utils.py
@@ -1,6 +1,7 @@
 # TODO: Update it to the new persistency
+import glob
 
-
+import numpy  as np
 import tables as tb
 import pandas as pd
 
@@ -11,170 +12,191 @@ from typing import List
 #from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-import invisible_cities.core.system_of_units as units
-
-from fanal.mc.mc_io_functions  import load_mc_particles
-from fanal.mc.mc_io_functions  import load_mc_hits
-
+import invisible_cities.core.system_of_units    as units
+from   invisible_cities.io.mcinfo_io        import load_mchits_df
+from   invisible_cities.io.mcinfo_io        import load_mcparticles_df
 
 
-def print_mc_particles(mcParticles:  pd.DataFrame,
-                       mcHits:       pd.DataFrame,
-                       evt_ini_time: float = 0.,
-                       with_hits:    bool = False) -> None:
+
+def get_event_numbers_in_file(file_name: str) -> np.ndarray:
+    """
+    Get a list of the event numbers in the file
+    based on the MC tables
+
+    parameters
+    ----------
+    file_name : str
+                File name of the input file
+
+    returns
+    -------
+    evt_arr   : np.ndarray
+                numpy array containing the list of all
+                event numbers in file.
+    """
+    with tb.open_file(file_name, 'r') as h5in:
+
+        def get_event_ids_table(tablename):
+            try:
+                evt_list = getattr(h5in.root.MC, tablename).cols.event_id
+            except tb.exceptions.NoSuchNodeError:
+                raise AttributeError('Trying to get event number from MC corrupted file.')
+            return np.unique(evt_list).astype(int)
+
+        evt_list = get_event_ids_table('particles')
+        if len(evt_list):
+            return evt_list
+        return get_event_ids_table('sns_response')
+
+
+
+def get_fname_with_event(event_id : int,
+                         ifnames  : str
+                        ) -> str :
+    """
+    It returns the mc ifname from all the ifnames
+    containing the event_id
+    """
+    fnames = glob.glob(ifnames)
+    if (len(fnames) == 0):
+        print("Input files do NOT EXIST.")
+        return ''
+    for fname in fnames:
+        if (event_id in get_event_numbers_in_file(fname)): return fname
+    return ''
+
+
+
+def print_mc_event(event_id  :  int,
+                   ifnames   :  List[str],
+                   with_hits :  bool = False
+                  ) -> None :
+    """Prints the information of the event corresponding to event_id."""
     
-    part_tab = ' ' * 2
+    # Getting the right file
+    ifname = get_fname_with_event(event_id, ifnames)
+    if ifname == '':
+        print(f"Event id: {event_id} NOT FOUND in input mc files.")
+        return
+    else:
+        print(f"Event Id: {event_id}  contained in {ifname}\n")
+
+    # Getting the mcParticles and mcHits of the right event
+    mcParts = load_mcparticles_df(ifname).loc[event_id]
+    mcHits  = load_mchits_df(ifname).loc[event_id]
+
+    print_mc_particles(mcParts, mcHits, with_hits)
+
+    return
+
+
+
+def print_mc_particles(mcParts   : pd.DataFrame,
+                       mcHits    : pd.DataFrame,
+                       with_hits : bool  = False
+                      ) -> None:
     
-    for part_id, part in mcParticles.iterrows():
+    print(f"*** {len(mcParts)} MC particles:")
+    #print(mcParts)
+
+    t0 = mcHits.time.min()
+
+    for part_id, part in mcParts.iterrows():
         # General Info
-        print()
-        print(part_tab + 'Particle {0}: name = {1},   primary = {2}'
-              .format(part_id, part['name'], part.primary))
-        
-        # Creator Info
-        if not part.primary:
-            mother_part = mcParticles.loc[part.mother_id, :]
-            print(part_tab + 'Prod.: Process = {0},   Mother index = {1} ({2})'
-                  .format(part.creator_proc, part.mother_id, mother_part['name']))
+        print(f"* Particle {part_id}: name = {part.particle_name},   primary = {part.primary}")
 
-        # Production Info
-        print(part_tab + 'Prod.: Mom = ({0:.1f}, {1:.1f}, {2:.1f}) keV,   KinE: {3:.1f} keV'
-             .format(part.ini_px/units.keV, part.ini_py/units.keV,
-                     part.ini_pz/units.keV, part.kin_energy/units.keV))
-        print(part_tab + 'Prod.: Volume = {0},   Vertex = ({1:.1f}, {2:.1f}, {3:.1f}) mm'
-              .format(part.initial_volume, part.ini_x, part.ini_y, part.ini_z))
-        
+        # Creator Info for non primary particles
+        if not part.primary:
+            mother_part = mcParts.loc[part.mother_id]
+            print(f"  Initial:  Process = {part.creator_proc},   Mother id = " + \
+                  f"{part.mother_id} ({mother_part.particle_name})")
+
+        # Initial Info
+        print(f"  Initial:  Momentum = ({part.initial_momentum_x/units.keV:.1f}, " + \
+              f"{part.initial_momentum_y/units.keV:.1f}, {part.initial_momentum_z/units.keV:.1f})" + \
+              f" keV  ->  KinE: {part.kin_energy/units.keV:.1f} keV")
+        print(f"  Initial:  Volume = {part.initial_volume}   Vertex = ({part.initial_x:.1f}," + \
+              f" {part.initial_y:.1f}, {part.initial_z:.1f}) mm") 
+
         # Decay Info
-        print(part_tab + 'Decay: Volume = {0},   Vertex = ({1:.1f}, {2:.1f}, {3:.1f}) mm'
-              .format(part.final_volume, part.final_x, part.final_y, part.final_z))
+        print(f"  Decay  :  Volume = {part.final_volume}   Vertex = ({part.final_x:.1f}," + \
+              f" {part.final_y:.1f}, {part.final_z:.1f}) mm")
+        print(f"  Decay  :  Process = {part.final_proc}   " + \
+              f"Momentum = ({part.final_momentum_x/units.keV:.1f}, " + \
+              f"{part.final_momentum_y/units.keV:.1f}, {part.final_momentum_z/units.keV:.1f}) keV")
 
         # Daughter Particles Info
-        daughter_parts = mcParticles[mcParticles.mother_id == part_id]
-        print(part_tab + '{} daughter particles:'.format(len(daughter_parts)))
+        daughter_parts = mcParts[mcParts.mother_id == part_id]
+        print(f"  {len(daughter_parts)} daughter particles:")
+
         for daugh_id, daugh_part in daughter_parts.iterrows():
-            print(part_tab*2, 'Part {0}   Name: {1:10},   Volume: {2}'.format(daugh_id,
-                                                                              daugh_part['name'],
-                                                                              daugh_part.initial_volume))
-        
+            print(f"    Part id: {daugh_id:3} - {daugh_part.particle_name:8} - " + \
+                  f"{daugh_part.initial_volume:8} - {daugh_part.creator_proc:8}")
+
         # Printing hits
-        if with_hits:
-            if (part_id in mcHits.index.get_level_values(0)):
-                part_hits = mcHits.loc[part_id, :]
-                print(part_tab +'{} MC Hits:'.format(len(part_hits)))
-
-                for hit_id, hit in part_hits.iterrows():
-                    print(part_tab*2, 'Hit {:2}   Det: {},   E: {:5.1f} KeV   ({:5.0f}, {:5.0f}, {:5.0f}) mm,   t: {:.1e} us,   Evt. t: {:.1e} us'
-                          .format(hit_id, hit.label, hit.E/units.keV, hit.x, hit.y, hit.z,
-                                  hit.time/units.mus, (hit.time - evt_ini_time)/units.mus))
-
-            else:
-                print(part_tab +'0 MC Hits')
+        if (with_hits == True):
+            print_mc_hits(mcHits.loc[part_id], t0)
+        else:
+            print(f"  {len(mcHits)} MC hits")
 
 
 
-def print_mc_event(event_id:   int,
-                   iFileNames: List[str],
-                   with_hits:  bool = False
-                  ) -> None:
-    """Prints the information of the event corresponding to event_id.
-    It will look for it into all the list of iFileNames passed."""
-    
-    # Going through all the input files
-    for iFileName in iFileNames:
-        with tb.open_file(iFileName, mode='r') as h5in:
-            file_extents = pd.read_hdf(iFileName, '/MC/extents', mode='r')
+def print_mc_hits(mcHits : pd.DataFrame,
+                  t0     : float
+                 ) -> None :
 
-            if event_id in file_extents['evt_number'].tolist():
-                print('\nEvt Id: {}  contained in {}\n'.format(event_id, iFileName))
+    print(f"  {len(mcHits)} MC hits:")
+    #print(mcHits)
 
+    for hit_id, hit in mcHits.iterrows():
+        print(f"    Hit {hit_id:3}   Label: {hit.label}   E: {hit.energy/units.keV:6.3f} keV" + \
+              f"   Pos: ({hit.x:5.1f}, {hit.y:5.1f}, {hit.z:5.1f}) mm" + \
+              f"   time: {(hit.time - t0)/units.mus:.1e} us")
+
+
+
+
+
+
+
+
+#def plot_mc_event(event_id   : int,
+#	              iFileNames : List[str]
+#	             ) -> None:
+#    """
+#Plots the information of the event corresponding to event_id.
+#It will look for it into all the list of iFileNames passed."""
+#
+#    # Going through all the input files
+#    for iFileName in iFileNames:
+#        with tb.open_file(iFileName, mode='r') as h5in:
+#            file_extents = pd.read_hdf(iFileName, '/MC/extents', mode='r')
+#
+#            if event_id in file_extents['evt_number'].tolist():
+#                print('\nEvt Id: {}  contained in {}\n'.format(event_id, iFileName))
+#
 #                # Getting the mcParticles and mcHits of the right file
 #                file_mcHits  = load_mc_hits(iFileName)
-#                file_mcParts = load_mc_particles(iFileName)
+#                #file_mcParts = load_mc_particles(iFileName)
 #
 #                # Getting the mcParticles and mcHits of the right event
 #                event_mcHits       = file_mcHits.loc[event_id, :]
 #                active_mcHits      = event_mcHits[event_mcHits.label == 'ACTIVE']
-#                event_mcParticles  = file_mcParts.loc[event_id, :]
 #
-#                #Getting relevant info from the event
-#                event_mcE = active_mcHits.E.sum()
-#                ini_time  = active_mcHits.time.min()
-#                end_time  = active_mcHits.time.max()
-                
-                # Getting the mcParticles
-                file_mcParts = load_mc_particles(iFileName)
-                event_mcParticles  = file_mcParts.loc[event_id, :]
-
-                # Getting the mcHits and info related
-                if (with_hits):
-                    file_mcHits   = load_mc_hits(iFileName)
-                    event_mcHits  = file_mcHits.loc[event_id, :]
-                    active_mcHits = event_mcHits[event_mcHits.label == 'ACTIVE']
-                    # Getting relevant info from the event
-                    event_mcE = active_mcHits.E.sum()
-                    ini_time  = active_mcHits.time.min()
-                    end_time  = active_mcHits.time.max()
-                else:
-                    event_mcHits = []
-                    event_mcE = ini_time = end_time = 0.
-                                    
-                # General event info
-                print('  Event deposited energy = {0:.6f} MeV'.format(event_mcE))
-                print('  Event initial time = {0:.3e} us,   Time width: {1:.3e} us'
-                      .format(ini_time/units.mus, (end_time-ini_time)/units.mus))
-
-                print('  Event has {} MC Particles'.format(len(event_mcParticles)))
-                print('  Event has {} MC Hits'.format(len(event_mcHits)))
-                print('')
-                print('- List of MC Particles:')
-                
-                # Printing particles
-                print_mc_particles(event_mcParticles, event_mcHits,
-                                   evt_ini_time=ini_time, with_hits=with_hits)
-                
-                return
-
-    # Event Id not found in any input file
-    print('\nEvt Id: {}  NOT FOUND in any input file.\n'.format(event_id))
-
-
-
-def plot_mc_event(event_id   : int,
-	              iFileNames : List[str]
-	             ) -> None:
-    """
-Plots the information of the event corresponding to event_id.
-It will look for it into all the list of iFileNames passed."""
-
-    # Going through all the input files
-    for iFileName in iFileNames:
-        with tb.open_file(iFileName, mode='r') as h5in:
-            file_extents = pd.read_hdf(iFileName, '/MC/extents', mode='r')
-
-            if event_id in file_extents['evt_number'].tolist():
-                print('\nEvt Id: {}  contained in {}\n'.format(event_id, iFileName))
-
-                # Getting the mcParticles and mcHits of the right file
-                file_mcHits  = load_mc_hits(iFileName)
-                #file_mcParts = load_mc_particles(iFileName)
-
-                # Getting the mcParticles and mcHits of the right event
-                event_mcHits       = file_mcHits.loc[event_id, :]
-                active_mcHits      = event_mcHits[event_mcHits.label == 'ACTIVE']
-
-                # Plotting
-                fig = plt.figure()
-                ax = fig.add_subplot(111, projection='3d')
-                ax.set_xlabel('X (mm)')
-                ax.set_ylabel('Y (mm)')
-                ax.set_zlabel('Z (mm)')
-                p = ax.scatter(active_mcHits.x, active_mcHits.y, active_mcHits.z,
-                               cmap='coolwarm', c=(active_mcHits.E / units.keV))
-                cb = fig.colorbar(p, ax=ax)
-                cb.set_label('Energy (keV)')
-                plt.show()
-
-                return
-
-    # Event Id not found in any input file
-    print('\nEvt Id: {}  NOT FOUND in any input file.\n'.format(event_id))
+#                # Plotting
+#                fig = plt.figure()
+#                ax = fig.add_subplot(111, projection='3d')
+#                ax.set_xlabel('X (mm)')
+#                ax.set_ylabel('Y (mm)')
+#                ax.set_zlabel('Z (mm)')
+#                p = ax.scatter(active_mcHits.x, active_mcHits.y, active_mcHits.z,
+#                               cmap='coolwarm', c=(active_mcHits.E / units.keV))
+#                cb = fig.colorbar(p, ax=ax)
+#                cb.set_label('Energy (keV)')
+#                plt.show()
+#
+#                return
+#
+#    # Event Id not found in any input file
+#    print('\nEvt Id: {}  NOT FOUND in any input file.\n'.format(event_id))
+#
